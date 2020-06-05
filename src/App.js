6b58d6bcf40499c09f5cbe71af76c807969ae5d5
Michael Odusanya <modusanya12@gmail.com>
2020-06-05 14:47:05 -0400

    only lists the gif urls. Still got more to do

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import './App.css';
-import Giphy from './Components/Giphy'
+import Giphy from './Components/Giphy';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Giphy />
       </div>
-    )
+    );
   }
 }
 
--- src/Components/Giphy.js
+++ src/Components/Giphy.js
@@ -8,20 +8,61 @@ class Giphy extends Component {
         super(props)
     
         this.state = {
-            word: "",
-            gifs: [],
-                         
+            result: "",
+            searchInput: "",
+            gifs: [],             
         };
     }
-    
+
+    handleInput = (e) => {
+        this.setState({searchInput: e.target.value})
+    };
+
+    handleSearch = () => {
+        const searchInput = this.state.searchInput;
+        const API_Key = process.env.REACT_APP_APIKEY;
+        const url = `http://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=${API_Key}`;
+
+        axios.get(url, {params:{key: API_Key}})
+        .then((response)=>{
+            const data = response.data;
+            const gifs = data.data;
+            console.log(gifs);
+            this.setState({result: searchInput, searchInput: "", gifs});
+        })
+        .catch((err) => {
+            console.log(err);
+            this.setState({gifs:[]});
+        });
+    };
     
     
     render() {
-        return (
-            <div>
-                
+        let gifList;
+        if(this.state.gifs.length===0){
+            gifList = <></>;
+        }else{
+            gifList = (
+                <ul>
+                    {this.state.gifs.map((gif, index)=>{
+                        return <li key = {index}>{gif.url}</li>;
+                    })}
+                </ul>
+            );
+        }
+
+        return(
+            <div className = "giphy">
+                <h1>Gif Search</h1>
+                <Search 
+                    value = {this.state.searchInput}
+                    onChange = {this.handleInput}
+                    onSearch = {this.handleSearch}
+                />
+                <h3>{this.state.result}</h3>
+                {gifList}
             </div>
-        )
+        );
     }
 }
 
